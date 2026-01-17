98530538fac7017aeab31a843eaf86616d11cd6d
Juyear009 <githubbruny@gmail.com>
2026-01-17 23:32:11 +0900

    feat: create HeroSection and NavBar components(MVP)

--- about-me/src/app/page.tsx
+++ about-me/src/app/page.tsx
@@ -1,66 +1,34 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <div className={styles.HeroSection}>
+        <div>
+          <p className={styles.mainTitle1}>대학생 개발자</p>
+          <p className={styles.mainTitle1}>JUYEAR</p>
+        </div>
+
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          className={styles.profileImg}
+          height={450}
+          width={450}
+          src={"/profile.png"}
+          alt="프로필 사진"
         />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div className={styles.keywordSection}>
+        <p>코딩</p>
+        <p>UI/UX</p>
+        <p>디자인</p>
+        <p>창업</p>
+        <p>AI</p>
+        <p>INTJ</p>
+        <p>백준 플레티넘</p>
+        <p>남자</p>
+        <p>20살</p>
+      </div>
     </div>
   );
 }
---
+++ about-me/src/components/NavBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  const [section, setSection] = useState<string>("");
+  const router = useRouter();
+
+  const routerPage = (page: string) => {
+    router.push(`/${page}`);
+    setSection(page);
+  };
+
+  useEffect(() => {
+    console.log(section);
+  }, [section]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.homeNav}>
+        <p onClick={() => routerPage("")}>ABOUT</p>
+      </div>
+      <div className={styles.pageNav}>
+        <p
+          className={`${section === "project" ? styles.selectPage : ""}`}
+          onClick={() => routerPage("project")}
+        >
+          PROJECT
+        </p>
+        <p
+          className={`${section === "blog" ? styles.selectPage : ""}`}
+          onClick={() => routerPage("blog")}
+        >
+          BLOG
+        </p>
+        <p
+          className={`${section === "certified" ? styles.selectPage : ""}`}
+          onClick={() => routerPage("certified")}
+        >
+          CERTIFIED
+        </p>
+        <p
+          className={`${section === "instagram" ? styles.selectPage : ""}`}
+          onClick={() => routerPage("instagram")}
+        >
+          INSTAGRAM
+        </p>
+      </div>
+      <div className={styles.supportNav}>
+        <p>문의하기</p>
+      </div>
+    </div>
+  );
+}
