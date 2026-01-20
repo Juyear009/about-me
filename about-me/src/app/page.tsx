d5c7a648b0e3b049fa0904c681ba029e8b198ff2
Juyear009 <githubbruny@gmail.com>
2026-01-20 22:24:50 +0900

    feat: implement profile summary section

--- about-me/src/app/page.tsx
+++ about-me/src/app/page.tsx
@@ -62,6 +62,53 @@ export default function Home() {
           ))}
         </div>
       </div>
+      <div className={styles.profileSummary}>
+        <div className={styles.profileSummaryHeading}>
+          <p className={styles.profileSummaryTitle}>
+            FULLSTACK <br />
+            DEVELOPER
+            <br />
+            AI RESEARCHER
+          </p>
+          <p className={styles.profileSummaryDes}>
+            저는 다가오는 기술에 빠르게 적응하며,
+            <br />
+            단순히 기술을 활용하는 개발자가 아닌 <br />
+            문제를 해결하는 개발자로 성장하고자 합니다.
+          </p>
+        </div>
+        <div className={styles.profileSummaryBoxes}>
+          <div className={styles.profileSummaryBox1}>
+            <p className={styles.boxTitle}>DONGTAN</p>
+            <p className={styles.boxDes}>LOCATION</p>
+          </div>
+          <div className={styles.profileSummaryBox2}>
+            <p className={styles.boxTitle}>7+</p>
+            <p className={styles.boxDes}>YEARS OF EXPREIENCE</p>
+          </div>
+          <div className={styles.profileSummaryBox1}>
+            <p className={styles.boxTitle}>2005.07.28</p>
+            <p className={styles.boxDes}>DATE OF BIRTH</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.skillSection}>
+        <div className={styles.skillSectionHeading}>
+          <Image
+            src={"/red_button.svg"}
+            width={50}
+            height={50}
+            alt="빨간 아이콘"
+          />
+          <p className={styles.skillSectionTitle}>SKILLS</p>
+          <p className={styles.skillSectionDes}>
+            현재 기술 스택과 배우고 있는 기술을 정리해봤어요.
+          </p>
+        </div>
+        <div className={styles.skillBoxes}>
+          <div className={styles.skillBox}></div>
+        </div>
+      </div>
     </div>
   );
 }
