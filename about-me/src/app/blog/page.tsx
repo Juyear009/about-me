dc44876d66b07e6359972211dfd91196c7baffdd
Juyear009 <githubbruny@gmail.com>
2026-09-05 02:08:57 +0900

    fix: blog page text

--- about-me/src/app/blog/page.tsx
+++ about-me/src/app/blog/page.tsx
@@ -7,6 +7,9 @@ export default function Page() {
         <p className={styles.title}>기술로 다가가는 생각.</p>
         <p className={styles.des}>JUYEAR의 최신 블로그 글을 확인해보세요.</p>
       </div>
+      <div>
+        <p>준비중입니다...</p>
+      </div>
     </div>
   );
 }
